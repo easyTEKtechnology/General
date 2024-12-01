6206226f8a8120955b25438e2d692c8e91e5d95f
easyTEK.technology <services@easytek.technology>
2024-11-30 21:42:44 -0700

    Added PowerShell Script

---
+++ Disconnect-DomainToWorkgroup.ps1
@@ -0,0 +1,10 @@
+﻿Function Disconnect-DomainToWorkgroup
+    {
+        Remove-Computer -UnjoinDomaincredential $Domain\$DomainAccount -PassThru -Verbose -Restart
+    }
+
+$Domain = Read-Host "Enter Domain";
+
+$DomainAccount = Read-Host "Enter Domain Administrator Account Name";
+
+Disconnect-DomainToWorkgroup;
